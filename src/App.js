1bfbe5e84c238d8ff1da2b155c8e5540b0b7f9fb
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-10-03 10:15:58 -0500

    Creando carrusel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './App.css'
 
-function App() {
+const images=[
+  "https://cdn.pixabay.com/photo/2017/02/13/08/54/brain-2062057_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2014/11/24/18/50/mind-544404_960_720.png",
+  "https://cdn.pixabay.com/photo/2016/05/10/21/50/meditation-1384758_960_720.jpg"
+]
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Slide duration={3000}>
+     {
+        images.map((image, index) => (
+          <div>
+            <div key={index} className='each-slide-effect' >
+              <img src={image} className='image' />
+
+              <a href='https://www.google.com' target="_blank">Ver más</a>
+            </div>
+          </div>
+        )
+        )
+      }
+     </Slide>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
